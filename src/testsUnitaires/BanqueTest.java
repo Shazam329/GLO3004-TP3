61a9f95b364a6ceed10c6e9972ec4f6f05451d44
Mathieu L'Écuyer <mathieu.lecuyer.1@ulaval.ca>
2016-04-20 15:42:42 -0400

    Début des classes Tests BanqueTest et TransactionsTest

---
+++ src/testsUnitaires/BanqueTest.java
@@ -0,0 +1,69 @@
+package testsUnitaires;
+
+import org.junit.Test;
+import tp.*;
+
+import static org.junit.Assert.*;
+import static tp.Constantes.*;
+
+/**
+ *  Nom de la classe: BanqueTest
+ *  Description: Tests unitaires de la classe Banque
+ */
+public class BanqueTest {
+
+    @Test
+    public void ouvrirCompte() throws Exception {
+
+    }
+
+    @Test
+    public void fermerCompte() throws Exception {
+
+    }
+
+    @Test
+    public void supprimerCompte() throws Exception {
+
+    }
+
+    @Test
+    public void retraitC() throws Exception {
+
+    }
+
+    @Test
+    public void depotC() throws Exception {
+
+    }
+
+    @Test
+    public void depotLC() throws Exception {
+
+    }
+
+    @Test
+    public void virementC() throws Exception {
+
+    }
+
+    @Test
+    public void ch_NIP() throws Exception {
+
+    }
+
+    @Test
+    public void bilanV() throws Exception {
+
+    }
+
+    @Test
+    public void transactionSortante() throws Exception {
+
+    }
+
+    @Test
+    public void transactionEntrante() throws Exception {
+
+    }
+}
---
+++ src/testsUnitaires/TransactionsTest.java
@@ -0,0 +1,19 @@
+package testsUnitaires;
+
+import org.junit.Test;
+import tp.*;
+
+import static org.junit.Assert.*;
+import static tp.Constantes.*;
+
+/**
+ *  Nom de la classe: TransactionTest
+ *  Description: Tests unitaires de la classe TransactionTest
+ */
+public class TransactionsTest {
+
+    @Test
+    public void transactionInter() throws Exception {
+
+    }
+}
