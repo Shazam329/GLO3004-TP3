1fe8c8324b4324a257bec09cc070ef6eb5cba6cf
Mathieu L'Écuyer <mathieu.lecuyer.1@ulaval.ca>
2016-04-20 14:34:12 -0400

    classe Transactions complétée : méthode transactionInter

--- src/tp/Transactions.java
+++ src/tp/Transactions.java
@@ -9,7 +9,10 @@ import static tp.Constantes.*;
  *               d'argent qui circulent lors d'un virement d'un compte d'une banque à un autre compte
  *               d'une autre banque.
  *  Invariants:
- *      -
+ *      - Le nombre de banques doit être compris entre 2 et le nombre maximum de banques
+ *      - Une banque ne peut-être associée à deux numéros différents
+ *      - Le solde global des montants qui sortent des banques doit être le même que celui reçu par ces banques
+ *        plus les frais.
  */
 public class Transactions {
 
@@ -30,6 +33,11 @@ public class Transactions {
         // Préconditions
         if (b.size() < 2 || b.size() > MAX_BANQUE)
             throw new IllegalArgumentException("Le nombre de banques doit être compris entre 2 et MAX_BANQUE.");
+
+        Set banquesSet = new HashSet<Banque>(banques.values());
+        if (banques.values().size() != banquesSet.size())
+            throw new IllegalStateException("Une banque ne peut être associée à deux numéros différents.");
+
 
         // Post-conditions
         this.banques = b;
@@ -69,10 +77,43 @@ public class Transactions {
     // Méthodes publiques //
     ////////////////////////
 
+    // Méthode permettant d'effectuer un virement entre deux comptes de deux banques
     public void transactionInter(NumBanque b1, NumBanque b2, NumCompte nc1, NumCompte nc2, int montant){
 
+        // Préconditions
+        if (montant <= 0)
+            throw new IllegalArgumentException("Le montant de la transaction doit être plus grand que zéro.");
+        if (montant - FRAIS_TRANSACTION_ENTRANTE <= 0)
+            throw new IllegalArgumentException("Le montant moins les frais de transaction doit être plus grand que zéro.");
+        if (b1.getNum() == b2.getNum())
+            throw new IllegalArgumentException("Les numéros des deux banques doivent être différents.");
+        if (!banques.containsKey(b1))
+            throw new IllegalArgumentException("Cette banque n'existe pas.");
+        if (!banques.containsKey(b2))
+            throw new IllegalArgumentException("Cette banque n'existe pas.");
+        if (!banques.get(b1).getComptes().containsKey(nc1.getNum()))
+            throw new IllegalArgumentException("Un compte avec ce numéro n'existe pas.");
+        if (!banques.get(b2).getComptes().containsKey(nc2.getNum()))
+            throw new IllegalArgumentException("Un compte avec ce numéro n'existe pas.");
+        if (banques.get(b1).getComptes().get(nc1.getNum()).getDateFermeture() != null)
+            throw new IllegalArgumentException("Le compte est déjà fermé.");
+        if (banques.get(b2).getComptes().get(nc2.getNum()).getDateFermeture() != null)
+            throw new IllegalArgumentException("Le compte est déjà fermé.");
+        if (banques.get(b1).getComptes().get(nc1.getNum()).getSolde() - montant - FRAIS_TRANSACTION_SORTANTE < MIN_SOLDE)
+            throw new IllegalArgumentException("Le solde du compte après transaction doit être plus grand ou égal au solde minimum.");
 
 
+        // Post-conditions
+        banques.get(b1).transactionSortante(nc1, montant);
+        banques.get(b2).transactionEntrante(nc2, montant);
+
+        sortiesC += (montant + FRAIS_TRANSACTION_SORTANTE);
+        depotsC += (montant - FRAIS_TRANSACTION_ENTRANTE);
+        frais += (FRAIS_TRANSACTION_SORTANTE + FRAIS_TRANSACTION_ENTRANTE);
+
+
+        // Vérifie les invariants
+        verifieInvariants();
     }
 
 }
